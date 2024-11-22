80ea1a31e699e1f634afcb88448de6bb0c3b99db
gabrielh-github <141324085+gabrielh-github@users.noreply.github.com>
2024-11-22 10:51:58 -0300

    COMITANDO
    COMITANDO

---
+++ escola/servidor/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '',
+    database: 'aulabd'
+});
+
+//Conectando ao banco de dados
+db.connect((erro) => {
+    if (erro) {
+        console.error('Erro ao conectar ao MySQL:', erro);
+    }else{
+        console.log('Conectado ao MySQL com sucesso!');
+    }
+});
+
+
+//Rota para cadastrar usuário
+app.post('/alunos', (req, res) =>{
+    const {nome, cidade, estado} = req.body;
+
+    const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
+    db.query(sql, [nome, cidade, estado], (err, result) => {
+        if (err){
+            return res.status(500).json({error: 'Erro ao cadastrar aluno !'});
+        }
+        res.status(201).json({message: 'Aluno cadastrado com sucesso!', id: result.insertID});
+    });
+});
+
+
+//Rota para consultar todos os usuários
+app.get('/alunos', (req, res) =>{
+    const sql = 'SELECT * FROM alunos';
+    
+    db.query(sql, (err, results) => {
+        if(err){
+            return res.status(500).json({error:'Erro ao consultar alunos'});
+        }
+        res.json(results);
+    });
+});
+
+//Rota para consultar um usuário específico pelo ID
+app.get('/alunos/:codigo', (req, res) => {
+    const {codigo} = req.params;
+
+    const sql ='SELECT * FROM alunos WHERE codigo = ?';
+    db.query(sql, [codigo], (err, results) => {
+        if(err){
+            console.error('Erro ao buscar usuário', err);
+            return res.status(500).json({error: 'Erro ao buscar alunos'});
+        }
+
+        res.json(results[0]); //Retorna o primeiro resultado (usuário encontrado)
+    });
+});
+
+
+//Rota para atualizar usuário
+app.put('/alunos/:codigo', (req, res) =>{
+    const {codigo} = req.params;
+    const {nome, cidade, estado} = req.body;
+
+    const sql = 'UPDATE alunos SET nome = ?, cidade = ?, estado = ? WHERE codigo = ?';
+
+    db.query(sql, [nome, cidade, estado, codigo,], (err) => {
+    if(err){
+        console.error('Erro ao atualizar usuário:', err);
+        return res.status(500).json({error:'Erro ao atualizar usuário'});
+    }
+    res.json({message: 'Aluno atualizado com sucesso!'});
+    });
+});
+
+//Rota para excluir usuário
+app.delete('/alunos/:codigo', (req, res) => {
+    const {codigo} = req.params;
+    const sql = 'DELETE FROM alunos whe parei aaqui'
+})
+
